362763bf549b375fbec81b3e2fabe749ec3f9724
Inderpal001 <inderpal.pixlerlab@gmail.com>
2024-06-04 10:22:53 +0530

    minor update

--- src/Components/Main/Results/Search/Search.jsx
+++ src/Components/Main/Results/Search/Search.jsx
@@ -1,10 +1,11 @@
 import { IoMdSearch } from "react-icons/io";
 import "./Search.scss";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 export default function Search({searchItem}) {
   return (
-    <div className="search"> <IoMdSearch className="search-icon"/> <a className="search-link" href="/">{searchItem}</a></div>
+    <div className="search"> <IoMdSearch className="search-icon"/> <Link className="search-link" to="/">{searchItem}</Link></div>
   )
 }
 
